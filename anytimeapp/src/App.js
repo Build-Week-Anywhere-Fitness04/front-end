82e885b0abd68fa642408eec632cf7833fca088a
Andrew Cummings <andrew.amalee@gmail.com>
2021-08-23 17:27:29 -0700

    adding login route

--- anytimeapp/src/App.js
+++ anytimeapp/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="flex mt-40">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          Anywhere Fitness
+          <div>
+            <Link to="/login">Login</Link>
+          </div>
+        </header>
+      </div>
+    </Router>
   );
 }
 
