e9c7b5b27d7131e71190a6bf80aa4c99142bcd68
sethblakley <sethblakley869@gmail.com>
2021-08-24 20:14:10 -0400

    test

--- anytimeapp/src/App.js
+++ anytimeapp/src/App.js
@@ -4,17 +4,8 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import * as yup from 'yup';
 import schema from './validation/schema';
 import Login from "./components/Login";
-<<<<<<< HEAD
-import Home from "./components/Home";
-
-// initialLoginErrors = {
-//   email: '',
-//   password: ''
-// }
-=======
 import Registration from "./components/Registration";
 import PrivateRoute from "./components/PrivateRoute";
->>>>>>> e9eadfa7018def97276569cea476540e44da1034
 
 function App() {
   // const [loginErrors, setLoginErrors] = useState(initialLoginErrors);
@@ -57,11 +48,7 @@ function App() {
           <PrivateRoute exact path="/classes" />
           <Route path="/registration" component={Registration} />
           <Route path="/login" component={Login} />
-<<<<<<< HEAD
-          <Route path="/home" component={Home} />
-=======
           <Route path="/" component={Login} />
->>>>>>> e9eadfa7018def97276569cea476540e44da1034
         </Switch>
       </div>
     </Router>
