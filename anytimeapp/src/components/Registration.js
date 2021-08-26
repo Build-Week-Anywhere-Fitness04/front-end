88286bb31af569ba0cf6b68e90940885e33b8b27
sethblakley <sethblakley869@gmail.com>
2021-08-26 06:21:44 -0400

    fb

--- anytimeapp/src/components/Registration.js
+++ anytimeapp/src/components/Registration.js
@@ -61,29 +61,25 @@ class Registration extends React.Component {
   render() {
     return (
       <h1>
-        <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-md">
-            <img
-              className="mx-auto h-20 w-auto"
-              src="https://www.kindpng.com/picc/m/58-588546_weight-barbell-png-transparent-background-dumbbell-png-png.png"
-              alt="Workflow"
-            />
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              Register your account
-            </h2>
-            <p className="mt-2 text-center text-sm text-gray-600">
-              Or{" "}
-              <a
-                href="http://localhost:3000/Login"
-                className="font-medium text-indigo-600 hover:text-indigo-500"
-              >
-                Sign in Here
-              </a>
-            </p>
-          </div>
+        <div className="flex flex-col justify-center min-h-screen py-12 bg-cover bg-gray-50 sm:px-6 lg:px-8" style={{ backgroundImage: `url(${'https://images.squarespace-cdn.com/content/56e487181d07c0743d227289/1580599661558-FI6JCNI15REZ265HY9R1/Canva+-+Modern+gym+interior+with+equipment.jpg?format=1500w&content-type=image%2Fjpeg'})` }}>
 
           <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-            <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <div className="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10">
+              <div className="sm:mx-auto sm:w-full sm:max-w-md">
+
+                <h2 className="mt-6 text-3xl font-extrabold text-center text-gray-900">
+                  Register your account
+                </h2>
+                <p className="mt-2 text-sm text-center text-gray-600">
+                  Or{" "}
+                  <a
+                    href="http://localhost:3000/Login"
+                    className="font-medium text-indigo-600 hover:text-indigo-500"
+                  >
+                    Sign in Here
+                  </a>
+                </p>
+              </div>
               <form
                 className="space-y-6"
                 action="#"
@@ -106,7 +102,7 @@ class Registration extends React.Component {
                       value={this.state.credentials.username}
                       onChange={this.handleChange}
                       required
-                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      className="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     />
                   </div>
                 </div>
@@ -127,7 +123,7 @@ class Registration extends React.Component {
                       value={this.state.credentials.password}
                       onChange={this.handleChange}
                       required
-                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      className="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     />
                   </div>
                 </div>
@@ -137,7 +133,7 @@ class Registration extends React.Component {
                     id="instructor-box"
                     name="instructor-box"
                     type="checkbox"
-                    className="text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                    className="text-indigo-600 border-gray-300 rounded focus:ring-indigo-500"
                   />
                   <label
                     htmlFor="instructor-box"
@@ -153,11 +149,11 @@ class Registration extends React.Component {
                       id="remember-me"
                       name="remember-me"
                       type="checkbox"
-                      className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                      className="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500"
                     />
                     <label
                       htmlFor="remember-me"
-                      className="ml-2 block text-sm text-gray-900"
+                      className="block ml-2 text-sm text-gray-900"
                     >
                       Remember me
                     </label>
@@ -176,7 +172,7 @@ class Registration extends React.Component {
                 <div>
                   <button
                     type="submit"
-                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                   >
                     Sign in
                   </button>
@@ -189,13 +185,13 @@ class Registration extends React.Component {
                     <div className="w-full border-t border-gray-300" />
                   </div>
                   <div className="relative flex justify-center text-sm">
-                    <span className="px-2 bg-white text-gray-500">
+                    <span className="px-2 text-gray-500 bg-white">
                       Or continue with
                     </span>
                   </div>
                 </div>
                 <form
-                  className="space-y-6 my-5"
+                  className="my-5 space-y-6"
                   action="#"
                   method="POST"
                   onSubmit={this.login}
@@ -203,18 +199,18 @@ class Registration extends React.Component {
                   <div>
                     <button
                       type="submit"
-                      className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
                       Google
                     </button>
                   </div>
                 </form>
 
-                <div className="mt-6 grid grid-cols-3 gap-3">
+                <div className="grid grid-cols-3 gap-3 mt-6">
                   <div>
                     <a
                       href="#"
-                      className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                      className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50"
                     >
                       <span className="sr-only">Sign in with Facebook</span>
                       <svg
@@ -235,7 +231,7 @@ class Registration extends React.Component {
                   <div>
                     <a
                       href="#"
-                      className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                      className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50"
                     >
                       <span className="sr-only">Sign in with Google</span>
                       <svg
@@ -252,7 +248,7 @@ class Registration extends React.Component {
                   <div>
                     <a
                       href="#"
-                      className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                      className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50"
                     >
                       <span className="sr-only">Sign in with GitHub</span>
                       <svg
